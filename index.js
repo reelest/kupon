8a871aad23f24fb631c3c80e3dc5cfff2440ab0b
ROWEND36 <rowendduke36@gmail.com>
2023-07-03 13:07:36 +0000

    feat: switch to local

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { registerRootComponent } from "expo";
+import btoa from "btoa";
+import { Buffer } from "buffer";
+import { TextEncoder, TextDecoder } from "util";
+import 'formdata-polyfill/formdata.min.js'
 
+global.btoa = btoa;
+global.Buffer = Buffer;
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 import App from "./App";
 Error.stackTraceLimit = 40;
 
